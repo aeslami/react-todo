032b6792a2530969ee92047fa536277ccf0be96d
Amir Harandi <a.eslami76@gmail.com>
2018-12-11 09:22:31 +0000

    Add items state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import Title from './components/Title';
 import SearchBox from './components/SearchBox';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { items: [] };
+  }
+
+  addItem = item => {
+    this.setState({ items: [...this.state.items, { text: item }] });
+  };
+
   render() {
     return (
       <Fragment>
