1f7db43aa72a8e4e7278b52ea3e4d951cab8a0ef
Amir Harandi <a.eslami76@gmail.com>
2018-12-12 19:00:18 +0000

    Add style to TodoList component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,39 @@
 import React from 'react';
 
 const TodoList = props => {
-  const items = props.items.map(item => <li key={item.text}>{item.text}</li>);
-  return <ul>{items}</ul>;
+  const items = props.items.map(item => (
+    <li
+      key={item.id}
+      style={{
+        height: '40px',
+        fontSize: '25px',
+        borderRadius: '50px',
+        listStyleType: 'none',
+        cursor: 'pointer'
+      }}
+      onClick={() => props.removeItem(item.id)}
+    >
+      {item.text}
+    </li>
+  ));
+
+  return (
+    <div>
+      {props.items.length > 0 && (
+        <ul
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'auto',
+            padding: '1em',
+            borderRadius: '50px',
+            border: '4px solid black'
+          }}
+        >
+          {items}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default TodoList;
